8e8ed577e7a6846e5da50085cc01552889af1a6a
Tuyen Ngo <tuyen.ngo@hyperlab.se>
2018-12-18 22:12:23 +0100

    Update cohesion, add alignment, and separation

--- src/Vector.purs
+++ src/Vector.purs
@@ -3,9 +3,8 @@ module Vector where
 import Data.List
 import Data.Show
 import Prelude
-import Prelude
-import Prelude
 
+import Data.Int (toNumber)
 import Data.Foldable (and)
 import Data.Function (($))
 import Data.Functor ((<$>))
@@ -13,11 +12,31 @@ import Data.List (length, filter, zipWith)
 import Data.Maybe (Maybe(..))
 import Data.Ring ((+), (*), add, mul, negate)
 import Data.Tuple (Tuple(..), fst)
-import Data.Tuple.Nested (T3, get1)
+import Data.Tuple.Nested (T3, get1, get2)
 import Data.Unfoldable (unfoldr)
 import Prelude ((<>), (/=), (<<<), (<), (/))
 
 data Vector a = V (List a)
+
+time :: Number
+time = 1.0
+weight_cohesion :: Number
+weight_cohesion = 1.0
+weight_alignment :: Number
+weight_alignment = 1.0
+weight_separation :: Number
+weight_separation = 1.0
+
+----- Some hardcoded boids -----
+x = V (1.0:2.0:Nil)
+y = V (3.0:4.0:Nil)
+z = V (5.0:6.0:Nil)
+
+boid1 = Tuple x (Tuple y z)
+boid2 = Tuple y (Tuple z x)
+boid3 = Tuple z (Tuple x y)
+
+listOfBoids = boid1:boid2:boid3:Nil
 
 
 vector :: forall a. Ring a => List a -> Vector a
@@ -66,11 +85,41 @@ type Boid         = T3 Position Velocity Acceleration
 visibilitySphere :: Boid -> List Boid -> Number -> List Boid
 visibilitySphere boid boids r = filter (\b' -> absV (get1 boid `subV` get1 b') < r  ) $ filter (\b -> b /= boid) boids
 
+averageVector :: List (Vector Number) -> Vector Number
+averageVector vecs = scalarMul scalar totalSum
+  where scalar = 1.0 / (toNumber $ length vecs)
+        totalSum = sumVectors vecs
 
-cohesion :: List Boid -> Position
-cohesion boids = foldr (addV <<< get1) (zeroV 0) boids -- / (length boids)
-  where vec  = head boids
+sumVectors :: List (Vector Number) -> Vector Number
+sumVectors vecs = foldr addV (zeroV dims) vecs
+  where vec  = head vecs
         dims = case vec of
-            Just boid   -> dimensions (fst boid)
+            Just value   -> dimensions value
             Nothing     -> 0
         dimensions (V xs) = length xs
+
+-- Takes a visibility sphere and calculates the mass centrum of the sphere
+massCentrum :: List Boid -> Vector Number
+massCentrum boids = averageVector $ get1 <$> boids
+
+
+-- Returns the cohesion component of the acceleration vector
+cohesion :: Boid -> List Boid -> Vector Number
+cohesion boid boids = scalarMul scalar distance
+  where scalar = 1.0 / (time * time)
+        distance = subV (massCentrum boids) (get1 boid)
+
+-- Returns the alignment component of the acceleration vector
+alignment :: List Boid -> Vector Number
+alignment boids = scalarMul scalar averageVelocity
+  where scalar = 1.0 / time
+        averageVelocity = averageVector $ get2 <$> boids
+
+-- Returns the separation component of the acceleration vector
+separation :: Boid -> List Boid -> Vector Number
+separation boid boids = scalarMul scalar totalDistance
+  where scalar = 1.0 / (time*time)
+        totalDistance = sumVectors $ subV boidPos <$> boidsPos
+        boidPos = get1 boid
+        boidsPos = get1 <$> boids
+
