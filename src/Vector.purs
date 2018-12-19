388c8c03177840821b8cbc1801f6a187b641d6e7
Felix Nordén <felixnorden@gmail.com>
2018-12-19 15:04:58 +0100

    Move Boid function ins boid module and add some vector tests

--- src/Vector.purs
+++ src/Vector.purs
@@ -1,53 +1,38 @@
 module Vector where
 
-import Data.List
-import Data.Show
-import Prelude
-
-import Data.Int (toNumber)
-import Data.Foldable (and)
+import Control.Applicative (pure)
+import Data.Boolean (otherwise)
+import Data.Eq (class Eq, (==))
+import Data.Foldable (and, sum)
 import Data.Function (($))
-import Data.Functor ((<$>))
-import Data.List (length, filter, zipWith)
+import Data.Functor ((<$>), map)
+import Data.Int (toNumber)
+import Data.List (List(..), foldr, head, length, zipWith, (:))
 import Data.Maybe (Maybe(..))
-import Data.Ring ((+), (*), add, mul, negate)
-import Data.Tuple (Tuple(..), fst)
-import Data.Tuple.Nested (T3, get1, get2)
+import Data.Ord ((<=))
+import Data.Ring ((+), (*), (-), add, mul, negate, class Ring)
+import Data.Semigroup ((<>))
+import Data.Semiring (class Semiring, zero, one)
+import Data.Show (class Show, show)
+import Data.Tuple (Tuple(..))
 import Data.Unfoldable (unfoldr)
-import Prelude ((<>), (/=), (<<<), (<), (/))
+import Math (round, sqrt)
+import Prelude ((/), bind, (<<<))
+import Test.QuickCheck.Arbitrary (class Arbitrary)
+import Test.QuickCheck.Gen (Gen, choose, listOf)
+import Type.Data.Boolean (kind Boolean)
 
 data Vector a = V (List a)
-
-time :: Number
-time = 1.0
-weight_cohesion :: Number
-weight_cohesion = 1.0
-weight_alignment :: Number
-weight_alignment = 1.0
-weight_separation :: Number
-weight_separation = 1.0
-
------ Some hardcoded boids -----
-x = V (1.0:2.0:Nil)
-y = V (3.0:4.0:Nil)
-z = V (5.0:6.0:Nil)
-
-boid1 = Tuple x (Tuple y z)
-boid2 = Tuple y (Tuple z x)
-boid3 = Tuple z (Tuple x y)
-
-listOfBoids = boid1:boid2:boid3:Nil
-
 
 vector :: forall a. Ring a => List a -> Vector a
 vector (a:as) = V $ a:as
 vector _      = V Nil
 
-zeroV :: Int -> Vector Number
+zeroV :: forall a. Semiring a => Int -> Vector a
 zeroV dim = V $ unfoldr nextZero dim
-  where   nextZero :: Int -> Maybe (Tuple Number Int)
+  where   nextZero :: Int -> Maybe (Tuple a Int)
           nextZero n | n <= 0     = Nothing
-                     | otherwise  = Just (Tuple 0.0 (n-1))
+                     | otherwise  = Just (Tuple zero (n-1))
 
 instance showVector :: (Show a) => Show (Vector a) where
   show (V nums) = "(" <> components nums <> ")" where
@@ -69,57 +54,39 @@ subV a b = addV a $ scalarMul (negate one) b
 -- mulV (V as) (V bs) = V (zipWith (*) as bs)
 
 scalarMul :: forall a. Semiring a => a -> Vector a -> Vector a
-scalarMul x (V bs) = V ((mul x) <$> bs)
+scalarMul s (V bs) = V (mul s <$> bs)
 
-absV :: forall a. Semiring a => Vector a -> a
-absV (V xs) = foldr (\x -> add (x*x)) zero xs
--- instance semiringVector :: Semiring Vector where
-  -- add = addV
-  -- mul = mulV
+absV :: Vector Number -> Number
+absV (V xs) = sqrt $ foldr (\x -> add (x*x)) zero xs
 
-type Acceleration = Vector Number
-type Position     = Vector Number
-type Velocity     = Vector Number
-type Boid         = T3 Position Velocity Acceleration
+dimensionsV :: forall a. Vector a -> Int
+dimensionsV (V xs) = length xs
 
-visibilitySphere :: Boid -> List Boid -> Number -> List Boid
-visibilitySphere boid boids r = filter (\b' -> absV (get1 boid `subV` get1 b') < r  ) $ filter (\b -> b /= boid) boids
-
-averageVector :: List (Vector Number) -> Vector Number
-averageVector vecs = scalarMul scalar totalSum
-  where scalar = 1.0 / (toNumber $ length vecs)
-        totalSum = sumVectors vecs
-
-sumVectors :: List (Vector Number) -> Vector Number
-sumVectors vecs = foldr addV (zeroV dims) vecs
+sumV :: List (Vector Number) -> Vector Number
+sumV vecs = foldr addV (zeroV dims) vecs
   where vec  = head vecs
         dims = case vec of
-            Just value   -> dimensions value
+            Just xs	    -> dimensionsV xs
             Nothing     -> 0
-        dimensions (V xs) = length xs
 
--- Takes a visibility sphere and calculates the mass centrum of the sphere
-massCentrum :: List Boid -> Vector Number
-massCentrum boids = averageVector $ get1 <$> boids
+averageV :: List (Vector Number) -> Vector Number
+averageV vecs = scalarMul scalar totalSum
+  where scalar = 1.0 / (toNumber $ length vecs)
+        totalSum = sumV vecs
 
+-- | Properties and tests
 
--- Returns the cohesion component of the acceleration vector
-cohesion :: Boid -> List Boid -> Vector Number
-cohesion boid boids = scalarMul scalar distance
-  where scalar = 1.0 / (time * time)
-        distance = subV (massCentrum boids) (get1 boid)
+rVector :: Gen (Vector Number)
+rVector = do list <- listOf 3 $ choose 0.0 10.0
+             pure $ V list
 
--- Returns the alignment component of the acceleration vector
-alignment :: List Boid -> Vector Number
-alignment boids = scalarMul scalar averageVelocity
-  where scalar = 1.0 / time
-        averageVelocity = averageVector $ get2 <$> boids
+instance arbVector :: Arbitrary (Vector Number) where
+  arbitrary = rVector
 
--- Returns the separation component of the acceleration vector
-separation :: Boid -> List Boid -> Vector Number
-separation boid boids = scalarMul scalar totalDistance
-  where scalar = 1.0 / (time*time)
-        totalDistance = sumVectors $ subV boidPos <$> boidsPos
-        boidPos = get1 boid
-        boidsPos = get1 <$> boids
+prop_absV :: Vector Number -> Boolean
+prop_absV (V xs) = (round $ absV (V xs)) == (round <<< sqrt <<< sum <<< map (\x -> x*x) $ xs)
 
+prop_addV :: Vector Number -> Vector Number -> Boolean
+prop_addV (V as) (V bs) = (addV (V as) (V bs)) == V (sumList as bs)
+  where sumList (x:xs) (y:ys) = (x+y) : sumList xs ys
+        sumList (_)    (_)    = Nil
