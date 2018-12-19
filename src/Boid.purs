5597b388f425883994098299ce2419db06429f76
Felix Nordén <felixnorden@gmail.com>
2018-12-19 18:06:14 +0100

    Add loop, which doesn't work...

--- src/Boid.purs
+++ src/Boid.purs
@@ -5,45 +5,45 @@ import Data.Function (($))
 import Data.Functor ((<$>))
 import Data.List (List(..), filter, zipWith, (:))
 import Data.Ring ((*))
-import Data.Tuple.Nested (Tuple3, get1, get2)
+import Data.Tuple (Tuple, fst, snd)
 import Prelude ((/), (<), (<<<))
 import Vector (Vector, absV, averageV, scalarMul, subV, sumV)
 
 
-type Acceleration = Vector Number
+-- type Acceleration = Vector Number
 type Position     = Vector Number
 type Velocity     = Vector Number
-type Boid         = Tuple3 Position Velocity Acceleration
+type Boid         = Tuple Position Velocity-- Acceleration
 
 
 visibilitySphere :: Boid -> List Boid -> Number -> List Boid
-visibilitySphere boid boids r = filter (\b -> absV (get1 boid `subV` get1 b) < r) <<< filter (\b -> b /= boid) $ boids
+visibilitySphere boid boids r = filter (\b -> absV (fst boid `subV` fst b) < r) <<< filter (\b -> b /= boid) $ boids
 
 
 -- Takes a visibility sphere and calculates the mass centrum of the sphere
 massCentrum :: List Boid -> Vector Number
-massCentrum boids = averageV $ get1 <$> boids
+massCentrum boids = averageV $ fst <$> boids
 
 
 -- Returns the cohesion component of the acceleration vector
 cohesion :: Number -> Boid -> List Boid -> Vector Number
 cohesion timeStep boid boids = scalarMul scalar distance
   where scalar = 1.0 / (timeStep * timeStep)
-        distance = subV (massCentrum boids) (get1 boid)
+        distance = subV (massCentrum boids) (fst boid)
 
 -- Returns the alignment component of the acceleration vector
 alignment :: Number -> List Boid -> Vector Number
 alignment timeStep boids = scalarMul scalar averageVelocity
   where scalar = 1.0 / timeStep
-        averageVelocity = averageV $ get2 <$> boids
+        averageVelocity = averageV $ snd <$> boids
 
 -- Returns the separation component of the acceleration vector
 separation :: Number -> Boid -> List Boid -> Vector Number
 separation timeStep boid boids = scalarMul scalar totalDistance
   where scalar = 1.0 / (timeStep * timeStep)
         totalDistance = sumV $ subV boidPos <$> boidsPos
-        boidPos = get1 boid
-        boidsPos = get1 <$> boids
+        boidPos = fst boid
+        boidsPos = fst <$> boids
 
 -- Returns the resulting acceleration vector for a boid 
 -- (c = cohesion, l = alignment, s = separation)
--- src/Main.purs
+++ src/Main.purs
@@ -1,12 +1,12 @@
 module Main where
 
-import Boid (Boid, acceleration, alignment, cohesion, separation)
+import Boid (Boid, acceleration, alignment, cohesion, separation, visibilitySphere)
 import Data.List (List(..), (:))
-import Data.Tuple.Nested (tuple3)
-import Effect (Effect)
-import Effect.Console (log)
-import Prelude (Unit)
-import Vector (Vector(..))
+import Data.Tuple (Tuple(..), fst, snd)
+import Effect (Effect, whileE)
+import Effect.Console (log, logShow)
+import Prelude (Unit(..), show, ($), (*), (<$>), discard, pure, unit, (+), bind)
+import Vector (Vector(..), scalarMul)
 
 -- Parameters for the model to be tweaked to our liking
 timeStep :: Number
@@ -16,10 +16,13 @@ weight_cohesion :: Number
 weight_cohesion = 1.0
 
 weight_alignment :: Number
-weight_alignment = 1.0
+weight_alignment = 0.5
 
 weight_separation :: Number
-weight_separation = 1.0
+weight_separation = 1.3
+
+visibility_radius :: Number
+visibility_radius = 10.0
 
 weightConstants :: List Number
 weightConstants = weight_cohesion : weight_alignment : weight_separation : Nil
@@ -34,13 +37,13 @@ z :: Vector Number
 z = V (5.0:6.0:Nil)
 
 boid1 :: Boid
-boid1 = tuple3 x y z
+boid1 = Tuple x y 
 
 boid2 :: Boid
-boid2 = tuple3 y z x
+boid2 = Tuple y z 
 
 boid3 :: Boid
-boid3 = tuple3 z x y
+boid3 = Tuple z x 
 
 listOfBoids :: List Boid
 listOfBoids = boid1:boid2:boid3:Nil
@@ -60,4 +63,24 @@ acceleration' = acceleration weightConstants
 
 main :: Effect Unit
 main = do
-  log "Hello world"
+  log "** Boid Simulation **"
+  next listOfBoids
+  log "** End of Simulation **"
+    where next :: List Boid -> Effect Unit
+          next boids = do logShow boids
+                          let nextBoids = iteration boids
+                          next nextBoids                     
+
+
+iteration :: List Boid -> List Boid
+iteration boids = iterate <$> boids
+  where iterate boid = Tuple pos' vel'
+          where c         = cohesion' boid vs
+                l         = alignment' vs
+                s         = separation' boid vs
+                acc       = acceleration' c l s
+                vs        = visibilitySphere boid boids visibility_radius
+                pos       = fst boid
+                vel       = snd boid
+                pos'	    = scalarMul (timeStep * timeStep) acc
+                vel'	    = scalarMul timeStep acc
