7a787701abea4b2fe18fe042f49275a313eef151
Tuyen Ngo <tuyen.ngo@hyperlab.se>
2018-12-20 14:43:21 +0100

    Try to animate boids

--- src/BoidDrawing.purs
+++ src/BoidDrawing.purs
@@ -1,36 +1,55 @@
 module BoidDrawing where
 
 import Prelude
-import Data.Maybe (Maybe(..))
-import Effect (Effect)
-import Graphics.Canvas (Context2D, fillPath, fillRect, arc, setFillStyle, getContext2D,
-                        getCanvasElementById)
-import Math (pi)
+
 import Data.Int (toNumber)
+import Data.List (List(..), (:), head, (!!), length)
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Tuple (fst)
+import Effect (Effect, forE)
+import Effect.Console (logShow)
+import Graphics.Canvas (Context2D, fillPath, fillRect, arc, setFillStyle, getContext2D, getCanvasElementById)
+import Math (pi)
 import Partial.Unsafe (unsafePartial)
+import Prelude (Unit, discard, ($), void, (*), bind, (<$>), pure, unit)
+import Vector (Vector(..), Vector)
+import Boid (Boid)
 
-type State =
-  { x::Number, y::Number }
+-- main :: Vector Number -> Effect Unit
+-- main position = void $ unsafePartial do 
+--   Just canvas <- getCanvasElementById "canvas"
+--   ctx <- getContext2D canvas
+--   clearCanvas ctx
+--   drawBoid ctx position
 
-main :: Effect Unit
-main = void $ unsafePartial do 
+main :: List (Vector Number) -> Effect Unit
+main positions = void $ unsafePartial do 
   Just canvas <- getCanvasElementById "canvas"
   ctx <- getContext2D canvas
-  clearCanvas ctx
-  drawBoid ctx { x: 400.0, y: 300.0 }
+  pure unit
 
 clearCanvas :: Context2D -> Effect Unit
 clearCanvas ctx = do
   setFillStyle ctx "#000000"
   fillRect ctx { x: 0.0, y: 0.0, width: 800.0, height: 600.0 }
 
-drawBoid :: Context2D -> State -> Effect Unit
-drawBoid ctx state = do
+drawBoids :: Context2D -> List Boid -> Effect Unit
+drawBoids ctx boids = do 
+  forE 0 (length boids) (\x -> do 
+    let positions = fst <$> boids
+    let position = fromMaybe (V (20.0:50.0:Nil)) (positions !! x)
+    drawBoid ctx position
+  )
+
+drawBoid :: Context2D -> Vector Number -> Effect Unit
+drawBoid ctx (V vector) = do
+  let x = fromMaybe 400.0 $ head vector
+  let y = fromMaybe 300.0 $ vector !! 1
   setFillStyle ctx "tomato"
   fillPath ctx $ arc ctx {
-    x: state.x,
-    y: state.y,
-    radius: 10.0,
+    x: x,
+    y: y,
+    radius: 3.0,
     start: 0.0,
     end: toNumber 2 * pi
   }
--- src/Main.purs
+++ src/Main.purs
@@ -1,57 +1,70 @@
 module Main where
 
-import Control.Monad.State
-import Control.Monad.State.Class
-import Prelude
+import Control.Monad.State (State)
+import Control.Monad.State.Class (put)
+import Prelude (Unit, bind, const, discard, pure, unit, ($), (*), (<$>), (<<<))
 
 import Boid (Boid, acceleration, alignment, cohesion, separation, visibilitySphere)
+import BoidDrawing as Drawing
 import Data.List (List(..), (:))
+import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..), fst, snd)
-import Effect (Effect, whileE)
+import Effect (Effect)
 import Effect.Console (log, logShow)
-import Effect.Ref (modify', modify_, new, read, write)
-import Prelude (Unit(..), show, ($), (*), (<$>), discard, pure, unit, (+), bind, (<<<))
+import Graphics.Canvas (Context2D, getCanvasElementById, getContext2D)
+import Prelude (Unit, bind, const, discard, pure, unit, ($), (*), (<$>), (<<<))
+import Signal as S
+import Signal.DOM as S
 import Vector (Vector(..), scalarMul)
 
 -- Parameters for the model to be tweaked to our liking
 timeStep :: Number
-timeStep = 1.0
+timeStep = 0.5
 
 weight_cohesion :: Number
-weight_cohesion = 1.0
+weight_cohesion = 0.15
 
 weight_alignment :: Number
 weight_alignment = 0.5
 
 weight_separation :: Number
-weight_separation = 1.3
+weight_separation = 0.3
 
 visibility_radius :: Number
-visibility_radius = 10.0
+visibility_radius = 100.0
 
 weightConstants :: List Number
 weightConstants = weight_cohesion : weight_alignment : weight_separation : Nil
 ----- Some hardcoded boids -----
 x :: Vector Number
-x = V (1.0:2.0:Nil)
+x = V (380.0:300.0:Nil)
 
 y :: Vector Number
-y = V (3.0:4.0:Nil)
+y = V (400.0:260.0:Nil)
 
 z :: Vector Number
-z = V (5.0:6.0:Nil)
+z = V (420.0:280.0:Nil)
+
+u :: Vector Number
+u = V (415.0:255.0:Nil)
+
+v :: Vector Number
+v = V (0.5: 1.0:Nil)
 
 boid1 :: Boid
-boid1 = Tuple x y 
+boid1 = Tuple x v 
 
 boid2 :: Boid
-boid2 = Tuple y z 
+boid2 = Tuple y v 
 
 boid3 :: Boid
-boid3 = Tuple z x 
+boid3 = Tuple z v 
+
+boid4 :: Boid
+boid4 = Tuple u v 
 
 listOfBoids :: List Boid
-listOfBoids = boid1:boid2:boid3:Nil
+listOfBoids = boid1:boid2:boid3:boid4:Nil
 
 initialState :: State (List Boid) Unit
 initialState = put listOfBoids
@@ -72,12 +85,15 @@ acceleration' = acceleration weightConstants
 main :: Effect Unit
 main = do
   log "** Boid Simulation **"
-  ref <- new listOfBoids
-  whileE (pure true) do
-    state <- read ref
-    logShow state
-    modify_ iteration ref 
-    pure unit
+  -- ref <- new listOfBoids
+  canvas <- getCanvasElementById "canvas"
+  case canvas of 
+    Just canvas -> do 
+      ctx <- getContext2D canvas
+      frames <- S.animationFrame
+      let simulation = S.foldp (const iteration) listOfBoids frames
+      S.runSignal (render ctx <$> simulation)
+    Nothing -> pure unit
   log "** End of Simulation **"
   
 
@@ -102,3 +118,9 @@ iteration boids = iterate <$> boids
                 vel       = snd boid
                 pos'	    = scalarMul (timeStep * timeStep) acc
                 vel'	    = scalarMul timeStep acc
+
+render :: Context2D -> List Boid -> Effect Unit
+render context state = do
+  Drawing.clearCanvas context
+  Drawing.drawBoids context state
+  pure unit
